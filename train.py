00c2ed417b10659bba7161435ceb685bd46149f6
lollo.zip <lorenzo.orsingher@gmail.com>
2023-06-23 18:32:46 +0200

    implement validation

--- train.py
+++ train.py
@@ -16,23 +16,30 @@ def next_batch(inputs, targets, batchSize):
 
 # specify our batch size, number of epochs, and learning rate
 BATCH_SIZE = 64
-EPOCHS = 100
-LR = 1e-2
+EPOCHS = 50
+LR = 1e-3
 # determine the device we will be using for training
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print("[INFO] training using {}...".format(DEVICE))
 
 data_path = "dataset/diabetes_prediction_dataset.csv"
 raw_data = load_data(data_path)
-X,y = polish_data(raw_data)
+ttdata = raw_data[:round(len(raw_data)-len(raw_data)*.10)]
+valdata = raw_data[round(len(raw_data)-len(raw_data)*.10):]
+X,y = polish_data(ttdata)
 X,y = balance_data(X,y)
-trainX, testX, valX, trainY, testY, valY = split_train_test(X,y,test_size=0.015, validation=True,val_size=0.10)
+trainX, testX, trainY, testY = split_train_test(X,y,test_size=0.015)
 trainX = torch.from_numpy(trainX).float()
 testX = torch.from_numpy(testX).float()
 trainY = torch.from_numpy(trainY).float()
 testY = torch.from_numpy(testY).float()
 
+Xv, yv = polish_data(valdata)
+valX, _, valY, _ = split_train_test(Xv,yv,test_size=0)
+valX = torch.from_numpy(valX).float()
+valY = torch.from_numpy(valY).float()
 
+#breakpoint()
 # initialize our model and display its architecture
 mlp = mlp.get_training_model().to(DEVICE)
 print(mlp)
@@ -50,6 +57,8 @@ for epoch in range(0, EPOCHS):
 	trainLoss = 0
 	trainAcc = 0
 	samples = 0
+	precision = 0
+	recall = 0
 	mlp.train()
 	chk = True
 	# loop over the current batch of data
@@ -58,7 +67,7 @@ for epoch in range(0, EPOCHS):
 		# model, and calculate loss
 		(batchX, batchY) = (batchX.to(DEVICE), batchY.to(DEVICE))
 		predictions = mlp(batchX)
-		
+		#print(predictions)
 		loss = lossFunc(predictions, batchY)
 		#breakpoint()
 		# zero the gradients accumulated from the previous steps,
@@ -68,6 +77,12 @@ for epoch in range(0, EPOCHS):
 		opt.step()
 		# update training loss, accuracy, and the number of samples
 		# visited
+
+
+
+		# precision += ((((predictions-.5) * (batchY-.5) > 0).sum().item() + 0.001)/(((predictions-.5) < 0).sum().item()+ 0.001))* batchY.size(0)
+		
+		# recall += ((((predictions-.5) * (batchY-.5) > 0).sum().item())/((batchY-.5) < 0).sum().item())* batchY.size(0)
 		trainLoss += loss.item() * batchY.size(0)
 		trainAcc += ((predictions-.5) * (batchY-.5) > 0).sum().item()
 		samples += batchY.size(0)
@@ -76,9 +91,9 @@ for epoch in range(0, EPOCHS):
 			chk = False
 	#breakpoint()
 	# display model progress on the current training batch
-	trainTemplate = "epoch: {} train loss: {:.3f} train accuracy: {:.3f}"
+	trainTemplate = "epoch: {} train loss: {:.3f} train accuracy: {:.3f} train precision: {:.3f} train recall: {:.3f}"
 	print(trainTemplate.format(epoch + 1, (trainLoss /samples ),
-		(trainAcc / samples)))
+		(trainAcc / samples), (precision/samples), (recall/samples)))
 	
 
 	# initialize tracker variables for testing, then set our model to
@@ -107,32 +122,28 @@ for epoch in range(0, EPOCHS):
 				chk = False
 			#breakpoint()
 		
-		# display model progress on the current test batch
-		# testTemplate = "epoch: {} test loss: {:.3f} test accuracy: {:.3f}"
-		# print(testTemplate.format(epoch + 1, (testLoss / samples),
-		# 	(testAcc / samples)))
+		#display model progress on the current test batch
+		testTemplate = "epoch: {} test loss: {:.3f} test accuracy: {:.3f}"
+		print(testTemplate.format(epoch + 1, (testLoss / samples),
+			(testAcc / samples)))
 		print("")
 
 print("SINGLE TRY")
 ################################
-raw_data= load_data(data_path)
-X,y = polish_data(raw_data)
 with torch.no_grad():
-	while True:
+
+		# flash the data to the current device
+		(batchX, batchY) = (valX.to(DEVICE), valY.to(DEVICE))
+		# run data through our model and calculate loss
+		predictions = mlp(batchX)
+		loss = lossFunc(predictions, batchY)
+		# update test loss, accuracy, and the number of
+		# samples visited
+		valLoss = loss.item() 
+		valAcc = ((predictions-.5) * (batchY-.5) > 0).sum().item()
 		
-		X = torch.from_numpy(np.asarray(X,dtype=np.float32)).float()
-		y =torch.from_numpy(np.asarray(y,dtype=np.float32)).float()
-
-		idx = randint(0,4000)
-
-		(bX,bY)=(X[idx],y[idx])
-
-
-		prediction = mlp(bX)
-		print(prediction)
-		if(y[idx] > 0.5):
-			print("diabetic")
-			breakpoint()
-		else:
-			print("not diabetic")
+		#display model progress on the current test batch
+		valTemplate = " val loss: {:.3f} val accuracy: {:.3f}"
+		print(valTemplate.format(valLoss,valAcc/len(valY)))
+		print("")
 ##############################
